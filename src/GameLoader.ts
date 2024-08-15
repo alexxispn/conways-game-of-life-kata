d4eb08d78c718ebaa4b0b2c0fc07cae89f4b6e70
Daute Rodríguez <dauter@ext.inditex.com>
2024-08-15 16:25:46 +0100

    green: loads a game with an empty final line

--- src/GameLoader.ts
+++ src/GameLoader.ts
@@ -2,7 +2,7 @@ import { GameOfLife } from "./GameOfLife.js"
 
 export class GameLoader {
   parse(input: string): GameOfLife {
-    const lines = input.split("\n")
+    const lines = input.trim().split("\n")
     const cells = lines.map((line) => line.split("").map((stringCell) => stringCell === "o"))
 
     return GameOfLife.from(cells)
