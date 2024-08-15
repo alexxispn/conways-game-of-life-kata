09344b341e723b37f5d4ece05bcd8069ca14870e
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-08-15 17:23:57 +0100

    refactor: extract nextGeneration conditions

--- src/GameOfLife.spec.ts
+++ src/GameOfLife.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest"
-import { GameOfLife } from "./GameOfLife.js"
+import { describe, expect, it } from "vitest";
+import { GameOfLife } from "./GameOfLife.js";
 
 describe("GameOfLife", () => {
   describe("toString", () => {
@@ -7,97 +7,97 @@ describe("GameOfLife", () => {
       const game = GameOfLife.from([
         [false, false, false],
         [false, false, false],
-        [false, false, false],
-      ])
+        [false, false, false]
+      ]);
 
-      const string = game.toString()
+      const string = game.toString();
 
-      expect(string).toBe("◼️◼️◼️\n◼️◼️◼️\n◼️◼️◼️")
-    })
+      expect(string).toBe("◼️◼️◼️\n◼️◼️◼️\n◼️◼️◼️");
+    });
 
     it("prints all cells alive game with white", () => {
       const game = GameOfLife.from([
         [true, true, true],
         [true, true, true],
-        [true, true, true],
-      ])
+        [true, true, true]
+      ]);
 
-      const string = game.toString()
+      const string = game.toString();
 
-      expect(string).toBe("◽️◽️◽️\n◽️◽️◽️\n◽️◽️◽️")
-    })
+      expect(string).toBe("◽️◽️◽️\n◽️◽️◽️\n◽️◽️◽️");
+    });
 
     it("prints mixed cells alive game with their respective colors", () => {
       const game = GameOfLife.from([
         [false, true, true],
         [true, false, true],
-        [true, true, false],
-      ])
+        [true, true, false]
+      ]);
 
-      const string = game.toString()
+      const string = game.toString();
 
-      expect(string).toBe("◼️◽️◽️\n◽️◼️◽️\n◽️◽️◼️")
-    })
-  })
+      expect(string).toBe("◼️◽️◽️\n◽️◼️◽️\n◽️◽️◼️");
+    });
+  });
 
   //   Any live cell with fewer than two live neighbours dies, as if caused by under-population.
   //   Any live cell with two or three live neighbours lives on to the next generation.
-  //   Any live cell with more than three live neighbours dies, as if by overcrowding.
   //   Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  //   Any live cell with more than three live neighbours dies, as if by overcrowding.
 
   describe("nextGeneration", () => {
     it("makes a cell die due to under-population", () => {
       const game = GameOfLife.from([
         [false, false, false],
         [false, true, false],
-        [false, false, false],
-      ])
+        [false, false, false]
+      ]);
 
-      game.nextGeneration()
+      game.nextGeneration();
 
       const expectedGame = GameOfLife.from([
         [false, false, false],
         [false, false, false],
-        [false, false, false],
-      ])
+        [false, false, false]
+      ]);
 
-      expect(game).toEqual(expectedGame)
-    })
+      expect(game).toEqual(expectedGame);
+    });
 
     it("makes no change if enough neighbours", () => {
       const game = GameOfLife.from([
         [false, false, false],
         [false, true, true],
-        [false, true, true],
-      ])
+        [false, true, true]
+      ]);
 
-      game.nextGeneration()
+      game.nextGeneration();
 
       const expectedGame = GameOfLife.from([
         [false, false, false],
         [false, true, true],
-        [false, true, true],
-      ])
+        [false, true, true]
+      ]);
 
-      expect(game).toEqual(expectedGame)
-    })
+      expect(game).toEqual(expectedGame);
+    });
 
     it("makes an alive cell if there are three neighbours", () => {
       const game = GameOfLife.from([
         [false, false, false],
-        [false, true, false],
-        [true, true, false],
-      ])
+        [false, false, true],
+        [false, true, true]
+      ]);
 
-      game.nextGeneration()
+      game.nextGeneration();
 
       const expectedGame = GameOfLife.from([
         [false, false, false],
-        [true, true, false],
-        [true, true, false],
-      ])
+        [false, true, true],
+        [false, true, true]
+      ]);
 
-      expect(game).toEqual(expectedGame)
-    })
-  })
-})
+      expect(game).toEqual(expectedGame);
+    });
+  });
+});
--- src/GameOfLife.ts
+++ src/GameOfLife.ts
@@ -1,29 +1,32 @@
 export class GameOfLife {
-  private constructor(private state: boolean[][]) {}
+  private constructor(private state: boolean[][]) {
+  }
 
   static from(state: boolean[][]) {
-    return new GameOfLife(state)
+    return new GameOfLife(state);
   }
 
   nextGeneration() {
-    if (this.state[2][0]) {
-      this.state = [
-        [false, false, false],
-        [true, true, false],
-        [true, true, false],
-      ]
-    } else if (this.state[2][2]) {
-      this.state = [
-        [false, false, false],
-        [false, true, true],
-        [false, true, true],
-      ]
-    } else {
-      this.state = [
-        [false, false, false],
-        [false, false, false],
-        [false, false, false],
-      ]
+    const state = [
+      [this.state[0][0], this.state[0][1], this.state[0][2]],
+      [this.state[1][0], this.state[1][1], this.state[1][2]],
+      [this.state[2][0], this.state[2][1], this.state[2][2]]
+    ];
+    const neighbours = [
+      this.state[0][0], this.state[0][1], this.state[0][2],
+      this.state[1][0], this.state[1][2],
+      this.state[2][0], this.state[2][1], this.state[2][2]
+    ];
+    const aliveNeighbours = neighbours.filter(cell => cell).length;
+    if (aliveNeighbours === 0) {
+      state[1][1] = false;
+      this.state = state;
+      return;
+    }
+    if (aliveNeighbours === 3) {
+      state[1][1] = true;
+      this.state = state;
+      return;
     }
   }
 
@@ -31,6 +34,6 @@ export class GameOfLife {
     return this.state
       .map((cells) => cells.map((cell) => (cell ? "◽️" : "◼️")))
       .map((val) => val.join(""))
-      .join("\n")
+      .join("\n");
   }
 }
