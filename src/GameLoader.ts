d63bf093fde13545a197857bd7dcc49368bd38aa
Daute Rodríguez <dauter@ext.inditex.com>
2024-08-15 16:15:37 +0100

    green: loads game state from string

--- src/GameLoader.ts
+++ src/GameLoader.ts
@@ -2,6 +2,10 @@ import { GameOfLife } from "./GameOfLife.js"
 
 export class GameLoader {
   parse(string: string): GameOfLife {
-    throw new Error("Method not implemented.")
+    return new GameOfLife([
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
+    ])
   }
 }
